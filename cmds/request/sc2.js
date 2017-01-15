ca30d11dcfa578bc0fb14e9bc0aae580b3702782
Ben Weier <ben.weier@gmail.com>
2017-01-16 07:56:07 +1000

    Expose the SC2 API command

---
+++ cmds/request/sc2.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs');
+
+const request = yargs
+  .command({
+    command: 'sc2 <resource>',
+    describe: 'Perform a SC2 request to the Battle.net API',
+    builder: (yargs) => yargs
+      .options({
+        origin: {
+          alias: 'o',
+          describe: 'The API endpoint to make the request to',
+          choices: ['us', 'eu', 'kr', 'tw', 'sea', 'cn'],
+          default: 'us',
+        },
+        locale: {
+          alias: 'l',
+          describe: 'A language locale code matching the origin endpoint',
+        },
+      })
+      .global(['origin', 'locale'])
+      .commandDir('sc2')
+      .demandCommand(1),
+    handler: (argv) => {
+    },
+  }).argv;
+
+module.exports = request;
