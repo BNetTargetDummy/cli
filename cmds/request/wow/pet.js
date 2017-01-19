95a64d2be596e72c0fa2e29d0164e7b325922a3c
Ben Weier <ben.weier@gmail.com>
2017-01-20 09:32:39 +1000

    Consolidate pet commands

--- cmds/request/wow/pet.js
+++ cmds/request/wow/pet.js
@@ -6,17 +6,46 @@ const blizzard = require('blizzard.js').initialize({ apikey: process.env.BATTLEN
 const request = yargs
   .command({
     command: 'pet',
-    describe: 'Fetch a World of Warcraft Pet',
-    builder: (yargs) => {
-      return yargs;
-    },
-    handler: (argv) => {
-      const { origin, locale } = argv;
-
-      return blizzard.wow.pet({ origin, locale })
-        .then(response => {
-          console.log(JSON.stringify(response.data));
+    describe: 'Fetch World of Warcraft pet data',
+    builder: yargs => {
+      return yargs
+        .options({
+          key: {
+            alias: 'k',
+            describe: 'The [key] of the {pet} data',
+            choices: ['list', 'ability', 'species', 'stats'],
+            default: 'list',
+            type: 'string',
+          },
+          id: {
+            alias: 'i',
+            describe: 'The [id] of the {pet} ability or species',
+            type: 'number',
+            demand: true,
+          },
+          level: {
+            alias: 'e',
+            describe: 'The pet level',
+            type: 'number',
+          },
+          breed: {
+            alias: 'd',
+            describe: 'The pet breed',
+            type: 'number',
+          },
+          quality: {
+            alias: 'q',
+            describe: 'The pet quality',
+            type: 'number',
+          },
         });
+    },
+    handler: argv => {
+      const { origin, locale, key, id, level, breed, quality } = argv;
+
+      return blizzard.wow.pet(key, { origin, locale, id, level, breedId: breed, qualityId: quality })
+        .then(response => console.log(JSON.stringify(response.data)))
+        .catch(err => console.log(JSON.stringify(err.response.data)));
     },
   }).argv;
 
