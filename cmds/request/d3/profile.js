bdb90b7c82f89aca8a90b23697fc162a80f2e26d
Ben Weier <ben.weier@gmail.com>
2017-01-19 12:06:52 +1000

    Consolidate hero request into profile command

--- cmds/request/d3/profile.js
+++ cmds/request/d3/profile.js
@@ -7,24 +7,28 @@ const request = yargs
   .command({
     command: 'profile',
     describe: 'Fetch a Diablo 3 Profile',
-    builder: (yargs) => {
+    builder: yargs => {
       return yargs
         .options({
           battletag: {
             alias: 'b',
             describe: 'The [battletag] of the {profile}',
             type: 'string',
+            demand: true,
           },
-        })
-        .demandOption(['battletag'], 'Please specify the [battletag] of the {profile}');
+          hero: {
+            alias: 'i',
+            describe: 'The [id] of the {hero}',
+            type: 'number',
+          },
+        });
     },
     handler: argv => {
-      const { origin, locale, battletag } = argv;
+      const { origin, locale, battletag, hero } = argv;
 
-      return blizzard.d3.profile({ origin, locale, battletag })
-        .then(response => {
-          console.log(JSON.stringify(response.data));
-        });
+      return blizzard.d3.profile({ origin, locale, tag: battletag, hero })
+        .then(response => console.log(JSON.stringify(response.data)))
+        .catch(err => console.log(JSON.stringify(err.response.data)));
     },
   }).argv;
 
