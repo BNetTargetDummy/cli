aa63bd0389ad847ccfe395228f4cabb0e891baba
Ben Weier <ben.weier@gmail.com>
2017-01-20 13:32:23 +1000

    Add D3 era and season commands

---
+++ cmds/request/d3/season.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs');
+const blizzard = require('blizzard.js').initialize({ apikey: process.env.BATTLENET_CLIENT_ID });
+
+const request = yargs
+  .command({
+    command: 'season',
+    describe: 'Fetch a Diablo 3 season',
+    builder: yargs => {
+      return yargs
+        .options({
+          id: {
+            alias: 'i',
+            type: 'number',
+          },
+          leaderboard: {
+            alias: 'b',
+            type: 'string',
+          },
+          token: {
+            alias: 't',
+            type: 'string',
+            demand: true,
+          },
+        });
+    },
+    handler: argv => {
+      const { origin, locale, id, leaderboard, token } = argv;
+
+      return blizzard.d3.season({ origin, locale, id, leaderboard, access_token: token })
+        .then(response => console.log(JSON.stringify(response.data)))
+        .catch(err => console.log(JSON.stringify(err.response.data)));
+    },
+  }).argv;
+
+module.exports = request;
